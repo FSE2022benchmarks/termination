795432af1be7e828c0532425210e9dd3720f4e8c
FSE2022benchmarks <101686496+FSE2022benchmarks@users.noreply.github.com>
2022-05-12 16:26:34 +0800

    Add files via upload

---
+++ Evalution/UAutomizer/loop/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+path='/home/ubuntu/tool/FSE_benchmark/loop'
+echo $path
+path_save='/home/ubuntu/tool/result/uau/loop'
+files=$(ls $path)
+for filename in $files
+do
+runfile=$path/$filename
+savefile=$path_save/$filename
+echo timeout 900 ./Ultimate.py --spec termination.prp --file  $filename  --architecture 64bit >> $savefile.txt
+echo ' ' >> $savefile.txt
+echo ' ' >> $savefile.txt
+echo ------------------------------------------------------------------------------------------------------- >> $savefile.txt
+echo ' ' >> $savefile.txt
+echo ' ' >> $savefile.txt
+echo 'testfile: '$filename >> $savefile.txt
+time timeout 900 /home/ubuntu/tool/uautomizer/UAutomizer-linux/Ultimate.py --spec termination.prp --file $runfile --full-output  --architecture 64bit >> $savefile.txt
+echo ' ' >> $savefile.txt
+echo finish $filename
+done
+
